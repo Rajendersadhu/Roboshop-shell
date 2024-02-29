635ef6ca3af5a5721d3b27923b6a709f3bd518b0
Rajendersadhu <Rajender2sr@gmail.com>
2024-02-29 20:48:24 +0530

    roboshop nginx script

--- redis.sh
+++ redis.sh
@@ -5,8 +5,9 @@ dnf module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 echo -e "\e[33mInstall Redis\e[0m"
 dnf install redis -y &>>/tmp/roboshop.log
 echo -e "\e[33mUpdate Listen Address\e[0m"
-sed -i 's/127.0.0.1 to 0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 echo -e "\e[33mStart & Enable Redis Service\e[0m "
 systemctl enable redis &>>/tmp/roboshop.log
 systemctl start redis &>>/tmp/roboshop.log
+
 
