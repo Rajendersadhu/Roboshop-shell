8b13c48e3b7ac35020eef0416ed7659304142b88
Rajendersadhu <Rajender2sr@gmail.com>
2024-02-29 21:02:01 +0530

    roboshop nginx script

--- redis.sh
+++ redis.sh
@@ -8,6 +8,7 @@ echo -e "\e[33mUpdate Listen Address\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
 echo -e "\e[33mStart & Enable Redis Service\e[0m "
 systemctl enable redis &>>/tmp/roboshop.log
-systemctl start redis &>>/tmp/roboshop.log
+systemctl restart redis &>>/tmp/roboshop.log
+
 
 
