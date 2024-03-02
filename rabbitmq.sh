49295dc35d1bcc7e6436a172e5acf369eaeb5303
Rajendersadhu <Rajender2sr@gmail.com>
2024-03-02 10:06:14 +0530

    roboshop nginx script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,13 +1,14 @@
 echo -e "\e[33mConfigure Erlang Repos\e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
 echo -e "\e[33mConfigure RabbitMQ Repos\e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
 echo -e "\e[33mInstall RabbitMQ\e[0m"
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y &>>/tmp/roboshop.log
 echo -e "\e[33mStart RabbitMQ Service\e[0m"
-systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
+systemctl enable rabbitmq-server &>>/tmp/roboshop.log
+systemctl restart rabbitmq-server &>>/tmp/roboshop.log
 echo -e "\e[33mAdd RabbitMQ Application User\e[0m"
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop roboshop123 &>>/tmp/roboshop.log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
+
 
