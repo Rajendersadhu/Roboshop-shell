4bd555608a73a38305c3d181efb2775d65886025
Rajendersadhu <Rajender2sr@gmail.com>
2024-03-18 00:54:24 +0530

    roboshop nginx script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,14 +1,25 @@
+source common.sh
 echo -e "${color}Configure Erlang Repos${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>$log_file
+stat_check $?
+
 echo -e "${color}Configure RabbitMQ Repos${nocolor}"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>$log_file
+stat_check $?
+
 echo -e "${color}Install RabbitMQ${nocolor}"
 dnf install rabbitmq-server -y &>>$log_file
+stat_check $?
+
 echo -e "${color}Start RabbitMQ Service${nocolor}"
 systemctl enable rabbitmq-server &>>$log_file
 systemctl restart rabbitmq-server &>>$log_file
+stat_check $?
+
 echo -e "${color}Add RabbitMQ Application User${nocolor}"
 rabbitmqctl add_user roboshop roboshop123 &>>$log_file
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$log_file
+stat_check $?
+
 
 
